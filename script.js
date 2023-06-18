e766bbd9b0bd859961b9b9e63e47817079bce5d6
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-06-18 15:55:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function volume_sphere(event) {
-  event.preventDefault(); // Prevent form submission
-  
-  // Get the radius input value
+  event.preventDefault();
+
   var radiusInput = document.getElementById('radius');
   var radius = parseFloat(radiusInput.value);
-  
-  // Calculate the volume
-  var volume = (4/3) * Math.PI * Math.pow(radius, 3);
-  
-  // Display the volume in the volume field
+
+  var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
   var volumeField = document.getElementById('volume');
-  volumeField.value = volume.toFixed(2); // Display volume with 2 decimal places
+  volumeField.value = volume.toFixed(4); // Display volume with 4 decimal places
 }
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+  document.getElementById('MyForm').onsubmit = volume_sphere;
+}
